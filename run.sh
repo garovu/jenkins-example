52c78fd6a94a553600764d82cdfd2d530abaedd6
Vũ Hoàng Nam <garovu.dev@gmail.com>
2024-03-20 20:35:19 +0700

    Update config

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+# create docker network
 docker network create jenkins
 
-
+# docker in docker
 docker run \
   --name jenkins-docker \
   --rm \
@@ -17,3 +17,18 @@ docker run \
   --publish 2376:2376 \
   docker:dind \
   --storage-driver overlay2
+
+# jenkins
+docker run \
+  --name jenkins-blueocean \
+  --restart=on-failure \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  myjenkins-blueocean:2.440.1-1
